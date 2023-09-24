340c983e18d7cad5310355ed13259ddd84ddfbd2
lyaaz <45838887+lyaaz@users.noreply.github.com>
2023-09-24 19:19:13 +0800

    fix zygisk broken

--- app/src/main/java/org/baiyu/fuckkiller/MainHook.java
+++ app/src/main/java/org/baiyu/fuckkiller/MainHook.java
@@ -8,8 +8,8 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class MainHook implements IXposedHookLoadPackage {
 
-    private final static int MAX_CACHED_PROCESSES = Integer.MAX_VALUE >> 4;
-    private final static int MAX_PHANTOM_PROCESSES = Integer.MAX_VALUE >> 4;
+    private final int MAX_CACHED_PROCESSES = Integer.MAX_VALUE / 16;
+    private final int MAX_PHANTOM_PROCESSES = Integer.MAX_VALUE / 16;
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
@@ -27,7 +27,7 @@ public class MainHook implements IXposedHookLoadPackage {
                 XposedHelpers.setIntField(param.thisObject, "mCustomizedMaxCachedProcesses", MAX_CACHED_PROCESSES);
                 XposedHelpers.setIntField(param.thisObject, "MAX_CACHED_PROCESSES", MAX_CACHED_PROCESSES);
                 XposedHelpers.setIntField(param.thisObject, "CUR_MAX_CACHED_PROCESSES", MAX_CACHED_PROCESSES);
-                XposedHelpers.setIntField(param.thisObject, "CUR_MAX_EMPTY_PROCESSES", MAX_CACHED_PROCESSES/2);
+                XposedHelpers.setIntField(param.thisObject, "CUR_MAX_EMPTY_PROCESSES", MAX_CACHED_PROCESSES / 2);
 
                 XposedHelpers.setIntField(param.thisObject, "MAX_PHANTOM_PROCESSES", MAX_PHANTOM_PROCESSES);
             }
