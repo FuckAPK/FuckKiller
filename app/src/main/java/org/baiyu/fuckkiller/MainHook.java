d44ca410fe3beaf64028ad6727741651cf365c6a
lyaaz <45838887+lyaaz@users.noreply.github.com>
2023-10-05 20:07:50 +0800

    update

--- app/src/main/java/org/baiyu/fuckkiller/MainHook.java
+++ app/src/main/java/org/baiyu/fuckkiller/MainHook.java
@@ -9,6 +9,8 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class MainHook implements IXposedHookLoadPackage {
 
     private static final Settings settings = Settings.getInstance();
+    private static final int maxCachedProcesses = Math.max(settings.getMaxCachedProcesses(), settings.getMinProcesses());
+    private static final int maxPhantomProcesses = Math.max(settings.getMaxPhantomProcesses(), settings.getMinProcesses());
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
@@ -18,30 +20,24 @@ public class MainHook implements IXposedHookLoadPackage {
 
         Class<?> activityManagerConstants = XposedHelpers.findClass("com.android.server.am.ActivityManagerConstants", lpparam.classLoader);
 
-        if (settings.getMaxCachedProcesses() >= settings.getMinProcesses()) {
-            XposedHelpers.setStaticIntField(activityManagerConstants, "DEFAULT_MAX_CACHED_PROCESSES", settings.getMaxCachedProcesses());
-            XposedBridge.hookAllConstructors(activityManagerConstants, new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    super.afterHookedMethod(param);
-                    XposedHelpers.setIntField(param.thisObject, "mCustomizedMaxCachedProcesses", settings.getMaxCachedProcesses());
-                    XposedHelpers.setIntField(param.thisObject, "MAX_CACHED_PROCESSES", settings.getMaxCachedProcesses());
-                    XposedHelpers.setIntField(param.thisObject, "CUR_MAX_CACHED_PROCESSES", settings.getMaxCachedProcesses());
-                    XposedHelpers.setIntField(param.thisObject, "CUR_MAX_EMPTY_PROCESSES", settings.getMaxCachedProcesses() / 2);
-                }
-            });
-        }
+        XposedHelpers.setStaticIntField(activityManagerConstants, "DEFAULT_MAX_CACHED_PROCESSES", maxCachedProcesses);
+        XposedBridge.hookAllConstructors(activityManagerConstants, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) {
+                XposedHelpers.setIntField(param.thisObject, "mCustomizedMaxCachedProcesses", maxCachedProcesses);
+                XposedHelpers.setIntField(param.thisObject, "MAX_CACHED_PROCESSES", maxCachedProcesses);
+                XposedHelpers.setIntField(param.thisObject, "CUR_MAX_CACHED_PROCESSES", maxCachedProcesses);
+                XposedHelpers.setIntField(param.thisObject, "CUR_MAX_EMPTY_PROCESSES", maxCachedProcesses / 2);
+            }
+        });
 
-        if (settings.getMaxPhantomProcesses() > settings.getMinProcesses()) {
-            XposedHelpers.setStaticIntField(activityManagerConstants, "DEFAULT_MAX_PHANTOM_PROCESSES", settings.getMaxPhantomProcesses());
-            XposedBridge.hookAllConstructors(activityManagerConstants, new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    super.afterHookedMethod(param);
-                    XposedHelpers.setIntField(param.thisObject, "MAX_PHANTOM_PROCESSES", settings.getMaxPhantomProcesses());
-                }
-            });
-        }
+        XposedHelpers.setStaticIntField(activityManagerConstants, "DEFAULT_MAX_PHANTOM_PROCESSES", maxPhantomProcesses);
+        XposedBridge.hookAllConstructors(activityManagerConstants, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) {
+                XposedHelpers.setIntField(param.thisObject, "MAX_PHANTOM_PROCESSES", maxPhantomProcesses);
+            }
+        });
 
         if (settings.isRecentTasksHookEnabled()) {
             Class<?> recentTasks = XposedHelpers.findClass("com.android.server.wm.RecentTasks", lpparam.classLoader);
@@ -54,8 +50,7 @@ public class MainHook implements IXposedHookLoadPackage {
                     boolean.class,
                     new XC_MethodHook() {
                         @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            super.beforeHookedMethod(param);
+                        protected void beforeHookedMethod(MethodHookParam param) {
                             param.args[2] = 0;
                         }
                     });
